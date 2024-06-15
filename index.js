0e45c6b86d029fda857eaf67509bb7e5664d2385
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-15 15:27:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// 1-sovol
+// alert( 5 % 2 );
+
+// 2-sovol
+//  let tahminison = Math.floor(Math.random() * 10);
+//  alert(tahminison)
+
+// 3-sovol
+// let son = Number(Math.floor(12.510));
+//  alert(son);
+
+// 4-sovol
+// function getlength (text) {
+//     return text.length;
+// }
+// let uzunlik = getlength("MARS IT SCHOOL");//12
+
+// alert(uzunlik);
+
+// 5-sovol
+// let mars = "MARS IT SCHOL";
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(mars)
+// }
+
+// 6-sovol
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ];
+// let ism = harflar[11] + harflar[7] + harflar[16] + harflar[9] + harflar[13] + harflar[11] + harflar[7] + harflar[10]
+
+// console.log(ism)
+
+// 7-sovol
+// let yosh = Number(prompt("Yoshingizni kiriting"));
+
+// if (yosh > 18) {
+//     console.log("Siz balog'atga yetgansiz!")
+// } else {
+//     console.log("Siz balog'atga yetmagansiz!")
+// }
+
+// 8-sovol
+// let Ism = prompt("Ismingizni kiriting")
+
+// Ism = Ism.split("")
+
+// Ism = Ism.reverse()
+// Ism = Ism.join("")
+
+// alert(Ism)
+
+// 10-sovol
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let toq = [];
+// let juft = [];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//   const element = sonlar[i];
+//   if (element % 2 === 0) {
+//     juft.push(element);
+//   } else {
+//     toq.push(element);
+//   }
+// }
+
+// console.log(juft);
+// console.log(toq)
+
